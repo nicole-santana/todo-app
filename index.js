6d7b5d4e87dfc3e957f65478471506c5e43ef9d0
nicole-santana <nicole.teles.santana@gmail.com>
2023-11-30 10:15:56 -0300

    iniciando leitura dos dados do banco de dados

--- index.js
+++ index.js
@@ -38,6 +38,24 @@ app.post('/criar', (requisicao, resposta) => {
 })
 
 app.get('/', (requisicao, resposta) => {
+    const sql = 'SELECT * FROM tarefas'
+
+    conexao.query(sql, (erro, dados) => {
+        if (erro) {
+            return console.log(erro)
+        }
+
+        console.log(dados)
+        
+        const tarefas = dados.map((dado) => {
+            return {
+                id: dado.id,
+                descricao: dado.descricao,
+                completa: dado.completa === 0 ? false : true
+            }
+        })
+    })
+
     resposta.render('home')
 })
 
